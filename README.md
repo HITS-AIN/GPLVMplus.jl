bf15993f7a7f146ff89f46e7d5a76227cc7f85fe
Nikos Gianniotis <ngiann@gmail.com>
2024-08-15 14:35:21 +0200

    first code commit

--- README.md
+++ README.md
@@ -1,3 +1,23 @@
 # GPLVMplus
 
 [![Build Status](https://github.com/ngiann/GPLVMplus.jl/actions/workflows/CI.yml/badge.svg?branch=main)](https://github.com/ngiann/GPLVMplus.jl/actions/workflows/CI.yml?query=branch%3Amain)
+
+
+```
+using GPLVMplusData # must be independently installed
+
+X = GPLVMplusData.loadducks(;every=4); # load rubber duck images in 32x32 resolution
+
+# warmup
+let
+    gplvmplus(X; Q = 2, iterations = 1)
+end
+
+# Learn mapping from Q=2 latent dimensions to high-dimensional images.
+# Use a two-hidden layer neural network for amortised inference. 
+result = GPLVMplus.gplvmplus(X; Q = 2, H1 = 20, H2 = 20, iterations = 5000);
+
+# Plot latent coordinates
+using PyPlot # must be independently installed
+plot(result[:Z][1,:],result[:Z][2,:],"o")
+```
--- src/GPLVMplus.jl
+++ src/GPLVMplus.jl
@@ -1,5 +1,38 @@
 module GPLVMplus
 
-# Write your package code here.
+    using LinearAlgebra, Distributions, Random, Statistics, Distances, LogExpFunctions
+    using ExponentialExpectations, FillArrays
+    using ForwardNeuralNetworks
+    using Printf, PyPlot
+    using JLD2
+    # using Artifacts, LazyArtifacts
+    using Transducers
+    using Optimization, OptimizationOptimJL, OptimizationBBO, Zygote#, LineSearches
 
+    # common
+    include("common/covariance.jl" )
+    include("common/woodbury.jl")
+    # include("common/rbf.jl")
+    include("common/myskip.jl")
+    include("common/inverterrors.jl")
+    include("common/toydata.jl")
+    include("common/expectation_latent_function_values.jl")
+    include("common/expectation_of_sum_D_log_prior_zero_mean.jl")
+    include("common/entropy.jl")
+    include("common/getfg!.jl")
+    include("common/callback.jl")
+
+    # GPLVM₊
+    include("gplvmplus/gplvmplus.jl")
+    include("gplvmplus/predictivesampler.jl")
+    include("gplvmplus/marginallikelihood.jl")
+    include("gplvmplus/marginallikelihood_VERIFY.jl")
+    include("gplvmplus/infertestlatent.jl")
+    include("gplvmplus/inferlightcurve.jl")
+    include("gplvmplus/infertestlatent_photo.jl")
+    include("gplvmplus/numerically_VERIFY.jl")
+    include("gplvmplus/unpack_gplvmplus.jl")
+    include("gplvmplus/unpack_inferlatent_gplvmplus.jl")
+    include("gplvmplus/partial_objective.jl")
+    
 end
